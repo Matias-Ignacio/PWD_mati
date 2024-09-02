5862afb6788bc7d6556d7840bf064f12f1d39096
Matias-Ignacio <matias.riveiro@est.fi.uncoma.edu.ar>
2024-09-02 00:33:09 -0300

    1-9 00h

--- Vista/Pruebas/Prueba/prueba.php
+++ Vista/Pruebas/Prueba/prueba.php
@@ -1,11 +1,10 @@
 <?php
-$titulo = "Ejercicio 1";
+$titulo = "Probandolo";
 include_once "../../Estructura/header.php";
 ?>
 <link rel="stylesheet" type="text/css" href="est.css">
 <script type="text/javascript" src="fun.js"></script>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
 
 <div class="divtitulo">
     <h1><?php echo $titulo;?></h1>
@@ -16,12 +15,12 @@ include_once "../../Estructura/header.php";
 <div class="container mt-3">
 <h1>Trabajo Obligatorio JS PEyLW 2024</h1>
 
-  <form id="miFormulario">
+  <form id="miFormulario" novalidate>
 
     <label for="nombre">Nombre:</label>
     <div class="input-group">
-        <span class="input-group-text">!</span>
-        <input class="form-control" type="text" id="nombre" name="nombre">
+      <input class="form-control" type="text" id="nombre" name="nombre">
+      <span class="input-group-text input-valido">X</span>
     </div>
 
     <label for="apellido">Apellido:</label>
@@ -30,15 +29,18 @@ include_once "../../Estructura/header.php";
       <span class="input-group-text">!</span>
     </div>
 
-    <label for="apellido">Email:</label>
-    <input type="text" id="email" name="email">
+    <label for="email">Email:</label>
+    <div class="input-group mb-3">
+      <input class="form-control" type="text" id="email" name="email">
+      <span class="input-group-text input-valido">@</span>
+    </div>  
 
-    <label for="obras_sociales">Obras Social:</label>
-    <select id="obras_sociales" name="obras_sociales">
-      <option value=""></option>
-      <option value="obra1">SOSUNC</option>
-      <option value="obra2">ISSN</option>
-      <option value="obra3">OSDE</option>
+    <label for="ciudad">Ciudad:</label>
+    <select id="ciudad" name="ciudad">
+      <option value="" selected disabled>Elige una opción...</option>
+      <option value="ciudad1">Neuquén</option>
+      <option value="ciudad2">Cipolletti</option>
+      <option value="ciudad3">Centenario</option>
       <!-- Agrega más opciones aquí -->
     </select>
 
@@ -63,34 +65,20 @@ include_once "../../Estructura/header.php";
 </div>
 
 
-
-<br/>
-
-<div id="enunciado">
-  <h1>Enunciado</h1>
-  Al presionar el boton "Enviar" deben realizarse las siguientes validaciones:
-
-  </div>
-
 <script> 
 
-  document.addEventListener('DOMContentLoaded', function() {
-    // Captura el formulario por su ID
-    var form = document.getElementById('miFormulario');
-
-    // Agrega un controlador de eventos para el evento 'submit'
-    form.addEventListener('submit', function(event) {
-      // Evita que el formulario se envíe
- 
-       // event.preventDefault();
-     
+  $(document).ready(function(){
+    $("nombre").blur(function(){
+      $(this).hide();
+      validarNombre(this);
     });
-  });   
+  });
 
-  document.getElementById("nombre").addEventListener("blur", function(){ validarNombre(this);});
+
+  //document.getElementById("nombre").addEventListener("blur", function(){ validarNombre(this);});
   document.getElementById("apellido").addEventListener("blur", function(){ validarNombre(this);});
-  document.getElementById("email").addEventListener("blur", validarEmail);
-  document.getElementById("obras_sociales").addEventListener("blur", validarObra);
+  //document.getElementById("email").addEventListener("blur", validarEmail);
+  document.getElementById("ciudad").addEventListener("blur", validarciudad);
   document.getElementById("dia").addEventListener("blur", validarFecha);
   document.getElementById("mes").addEventListener("blur", validarFecha);
   document.getElementById("anio").addEventListener("blur", validarFecha);
